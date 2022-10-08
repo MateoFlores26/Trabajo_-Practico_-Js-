8154425c23ff2d70097ba58c8e6f26300673a726
MateoFlores26 <floresmateojoaquin04@gmail.com>
2022-10-07 22:15:53 -0300

    Valido los valores

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,6 @@ function sumar(valor) {
     valor = parseInt(valor); // Convertir el valor a un entero (número).
 
     total = document.getElementById('spTotal').innerHTML;
+
+    // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
+    total = (total == null || total == undefined || total == "") ? 0 : total;
