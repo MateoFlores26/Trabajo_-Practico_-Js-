07ff78741351dbb4ac22b1f8b83687c28ef2c976
MateoFlores26 <floresmateojoaquin04@gmail.com>
2022-10-07 22:14:24 -0300

    Defino variables

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,6 @@
 /* Sumar dos números. */
 function sumar(valor) {
+    var total = 0;
+    valor = parseInt(valor); // Convertir el valor a un entero (número).
+
+    total = document.getElementById('spTotal').innerHTML;
